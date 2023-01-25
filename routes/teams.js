6fd743ebbdc428a0b3c5a1d77df328583e6b37eb
Mohamed Abdulle <m7assan95@gmail.com>
2023-01-25 17:31:02 -0500

    new router.delete

--- routes/teams.js
+++ routes/teams.js
@@ -6,4 +6,5 @@ router.get("/", teamsCtrl.index);
 router.get("/new", teamsCtrl.new);
 router.post("/", teamsCtrl.create);
 router.get("/:id", teamsCtrl.show);
+router.delete("/team/:id", teamsCtrl.delete);
 module.exports = router;
