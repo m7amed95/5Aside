678fd8d1311ecc5bd4d4e81d39a524cdd974bec8
Mohamed Abdulle <m7assan95@gmail.com>
2023-01-25 15:56:31 -0500

    new controller created

--- controllers/players.js
+++ controllers/players.js
@@ -1,1 +1,14 @@
 const Team = require("../models/team");
+
+module.exports = {
+  create,
+};
+
+function create(req, res) {
+  Team.findById(req.params.id, function (err, team) {
+    team.players.push(req.body);
+    team.save(function (err) {
+      res.redirect(`/teams/${team._id}`);
+    });
+  });
+}
