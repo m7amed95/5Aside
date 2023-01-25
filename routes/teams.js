5ccbe7efc55491b88765a1d6e511ca40c8504867
Mohamed Abdulle <m7assan95@gmail.com>
2023-01-25 15:51:04 -0500

    new player route created

--- routes/teams.js
+++ routes/teams.js
@@ -1,6 +1,9 @@
 var express = require("express");
 var router = express.Router();
 const teamsCtrl = require("../controllers/teams");
+const playerCtrl = require("../controllers/players");
+
+router.post("/teams/:id/players", playerCtrl.create);
 
 router.get("/", teamsCtrl.index);
 router.get("/new", teamsCtrl.new);
